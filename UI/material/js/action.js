8a701051ffe5dd5760168ad850646c64dc843326
ProfNaomi <nkoreste@gmail.com>
2019-02-26 21:51:05 +0300

    send this

--- UI/material/js/action.js
+++ UI/material/js/action.js
@@ -1,3 +1,19 @@
+/*// Create the XHR object.
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+    // XHR for Chrome/Firefox/Opera/Safari.
+    xhr.open(method, url, true);
+  } else if (typeof XDomainRequest != "undefined") {
+    // XDomainRequest for IE.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+  } else {
+    // CORS not supported.
+    xhr = null;
+  }
+  return xhr;
+} */
 //Display a slide show
 function reload() {
   var slides = document.getElementById("image-slides");
@@ -25,7 +41,82 @@ $("#form_reset").on("shown.bs.collapse", function() {
 let home_page = document.querySelector("#home_page");
 let main_page = document.querySelector("#main_page");
 let site_news = document.querySelector("#site_news");
-
+/*
+function getTitle(text) {
+  return text.match('<title>(.*)?</title>')[1];
+}
+*/
+/*function makeCorsRequest() {
+  // This is a sample server that supports CORS.
+  let url = "https://sokopoa.herokuapp.com/registration"
+  var xhr = createCORSRequest('POST', url);
+  if (!xhr) {
+    alert('CORS not supported');
+    return;
+  }
+  
+    // Response handlers.
+    xhr.onload = function() {
+      var text = xhr.responseText;
+      var title = getTitle(text);
+      alert('Response from CORS request to ' + url + ': ' + title);
+    };
+  
+    xhr.onerror = function() {
+      alert('Woops, there was an error making the request.');
+    };
+  
+    xhr.send();
+}*/
+//Create Account 
+function registration(){
+  let fname = document.querySelector('#fname').value;
+  let surname = document.querySelector('#surname').value;
+  let id = document.querySelector('#id').value;
+  let phone = document.querySelector('#phone').value;
+  let email = document.querySelector('#email').value;
+  let username = document.querySelector('#username').value;
+  let password = document.querySelector('#password').value;
+  let elem = document.querySelector('#element');
+  console.log(password);
+  $.ajax({
+    url:" http://127.0.0.1:5000/registration",
+    type:"POST",
+    crossDomain:true,
+    data:JSON.stringify({
+      fname:fname,
+      surname:surname,
+      id:id,
+      phone:phone,
+      email:email,
+      username:username,
+      password:password
+    }),
+    datatype:"json",
+    Success:function(response){
+      let response = JSON.parse(response);
+      if (response.success){
+        window.alert(response.message);
+        console.log(response.message);
+        $('#form_login').collapse("show");
+      } else{
+        window.alert(response.message);
+        fname = "";
+        surname = "";
+        id = "";
+        phone = "";
+        email = "";
+        username = "";
+        password = "";
+        console.log(response.message);
+      }
+    },
+    error: function (error) {
+      window.alert("An error occurred" + error);
+      console.log(error);
+  }
+  });
+};
 // login
 function login() {
   home_page.classList.add("d-none");
